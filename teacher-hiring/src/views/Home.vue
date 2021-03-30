731c77eb02372825bb3536c8934003c0bdf2f065
Thaodeclercq <56533772+Thaodeclercq@users.noreply.github.com>
2021-03-30 15:38:21 +0200

    Teacher hiring vue

---
+++ teacher-hiring/src/views/Home.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+  <div class="container">
+    <div class="container">
+      <div class="row">
+        <Item v-for="teacher in teachers" 
+        :key="teacher.id"
+        :item="teacher"/>
+      </div>
+    </div>  
+  </div>
+</template>
+	
+<script>
+// @ is an alias to /src
+import Item from "@/Item.vue"	
+export default {
+  name: 'Home',
+  computed: {
+    teachers() {
+      return this.$store.state.teachers;
+    }
+  },
+  components: {
+    Item
+  }
+}
+</script>
