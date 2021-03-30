22f729054aa1362c4d147c90755b910b6a52ee20
Eline <elineschepers1999@gmail.com>
2021-03-30 15:15:42 +0200

    added crud service class
    + readme deleted

--- src/main/java/com/example/demo/TeacherController.java
+++ src/main/java/com/example/demo/TeacherController.java
@@ -11,47 +11,7 @@ public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
-    /*@RequestMapping("/teachers")
-    public List<Teacher> getAllTeachers() {
-        List <Teacher> teachers = Arrays.asList(
-            new Teacher(
-                    "1",
-                    "Bram",
-                    "Lemeire",
-                    20,
-                    "good at teaching bla bla",
-                    "https://www.dropbox.com/s/swg9bdr0ejcbtrl/img9.jpg?raw=1",
-                    15
-            ),
-            new Teacher(
-                    "2",
-                    "Sander",
-                    "Verbeken",
-                    21,
-                    "good at teaching bla bla",
-                    "https://www.dropbox.com/s/6tqcep7rk29l59e/img2.jpeg?raw=1",
-                    10
-            ),
-            new Teacher(
-                    "3",
-                    "Eline",
-                    "Schepers",
-                    22,
-                    "good at teaching bla bla",
-                    "https://www.dropbox.com/s/78fot6w894stu3n/img3.jpg?raw=1",
-                    17
-            )
-        );
-        teachers.get(0).addCourseSubject("html");
-        teachers.get(0).addCourseSubject("css");
-        teachers.get(1).addCourseSubject("python");
-        teachers.get(1).addCourseSubject("java");
-        teachers.get(2).addCourseSubject("html");
-        teachers.get(2).addCourseSubject("css");
-        teachers.get(2).addCourseSubject("c++");
-
-        return teachers;
-    }*/
+    
 
     @RequestMapping("/teachers")
     public List<Teacher> getAllTeachers() {
@@ -74,7 +34,7 @@ public class TeacherController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/teachers/{id}")
-    public void deleteTeacher(@PathVariable Teacher id) {
+    public void deleteTeacher(@PathVariable String id) {
         teacherService.deleteTeacher(id);
     }
 
--- src/main/java/com/example/demo/TeacherService.java
+++ src/main/java/com/example/demo/TeacherService.java
@@ -10,50 +10,88 @@ import java.util.List;
 @Service
 public class TeacherService {
 
-    @Autowired
-    private TeacherRepository teacherRepository;
+    /*@Autowired
+    private TeacherRepository teacherRepository;*/
+    private List<Teacher> teachers = new ArrayList<>();
 
-    public List<Teacher> teachers = new ArrayList<>(Arrays.asList(
-        new Teacher(
-                "1",
-                "Bram",
-                "Lemeire",
-                20,
-                "good at teaching bla bla",
-                "https://www.dropbox.com/s/swg9bdr0ejcbtrl/img9.jpg?raw=1",
-                15
-        ),
-        new Teacher(
-                "2",
-                "Sander",
-                "Verbeken",
-                21,
-                "good at teaching bla bla",
-                "https://www.dropbox.com/s/6tqcep7rk29l59e/img2.jpeg?raw=1",
-                10
-        ),
-        new Teacher(
-                "3",
-                "Eline",
-                "Schepers",
-                22,
-                "good at teaching bla bla",
-                "https://www.dropbox.com/s/78fot6w894stu3n/img3.jpg?raw=1",
-                17
-        )
-    ));
-    
-    teachers.get(0).addCourseSubject("html");
-    teachers.get(0).addCourseSubject("css");
-    teachers.get(1).addCourseSubject("python");
-    teachers.get(1).addCourseSubject("java");
-    teachers.get(2).addCourseSubject("html");
-    teachers.get(2).addCourseSubject("css");
-    teachers.get(2).addCourseSubject("c++");
+    public void TeacherService() {
+        teachers.add(
+                new Teacher(
+                        "1",
+                        "Bram",
+                        "Lemeire",
+                        20,
+                        "good at teaching bla bla",
+                        "https://www.dropbox.com/s/swg9bdr0ejcbtrl/img9.jpg?raw=1",
+                        15
+                ));
+        teachers.add(
+                new Teacher(
+                        "2",
+                        "Sander",
+                        "Verbeken",
+                        21,
+                        "good at teaching bla bla",
+                        "https://www.dropbox.com/s/6tqcep7rk29l59e/img2.jpeg?raw=1",
+                        10
+                ));
+        teachers.add(
+                new Teacher(
+                        "3",
+                        "Eline",
+                        "Schepers",
+                        22,
+                        "good at teaching bla bla",
+                        "https://www.dropbox.com/s/78fot6w894stu3n/img3.jpg?raw=1",
+                        17
+                ));
 
+
+        teachers.get(0).addCourseSubject("html");
+        teachers.get(0).addCourseSubject("css");
+        teachers.get(1).addCourseSubject("python");
+        teachers.get(1).addCourseSubject("java");
+        teachers.get(2).addCourseSubject("html");
+        teachers.get(2).addCourseSubject("css");
+        teachers.get(2).addCourseSubject("c++");
+    }
 
     public List<Teacher> getAllTeachers() {
+        if(teachers.size()==0){TeacherService();}
         return teachers;
     }
 
+    public Teacher getTeacher(String id) {
+        for(Teacher t : teachers)
+        {
+            if(t.getId().equals(id)){
+                return t;
+            }
+        }
+        throw new IllegalArgumentException("de ID "+id+" is onbestaand");
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+    public void updateTeacher(String id, Teacher teacher)
+    {
+        for(int i=0; i<teachers.size(); i++)
+        {
+            if(teachers.get(i).getId().equals(id))
+            {
+                teachers.set(i,teacher);
+            }
+        }
+    }
+    public void deleteTeacher(String id)
+    {
+        for(int i=0; i<teachers.size(); i++)
+        {
+            if(teachers.get(i).getId().equals(id))
+            {
+                teachers.remove(i);
+            }
+        }
+    }
 }
